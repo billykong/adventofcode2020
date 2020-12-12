b1ea07cb7be4ccf61c19b2576c2dca3aacda03f0
Billy Kong <billyklh@gmail.com>
2020-12-12 15:57:17 +0800

    day9 q1

---
+++ day9/q1.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python3
+from input import input, test_input
+
+
+def parse_input(input):
+    intermediate = input.split()
+    return list(map(int, intermediate))
+
+def solution(input, preamble):
+    parsed_input = parse_input(input)
+    reference = []
+    for i, num in enumerate(parsed_input):
+        reference.append([])
+        for offset in range(1, min(preamble, i+1)):
+            reference[i].append(num + parsed_input[i - offset])
+
+        candidates = set()
+        for offset in range(0, min(preamble, i)):
+            tmp = preamble - offset
+            candidates.update({*reference[i-offset][0:tmp]})
+
+        if i >= preamble and num not in candidates:
+            return num
+
+
+print(solution(input, 25))
+
+assert(solution(test_input, 5) == 127)
