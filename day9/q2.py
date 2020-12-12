57fa3dd16e7d3bf0197ddb0e014c26e3f81058bd
Billy Kong <billyklh@gmail.com>
2020-12-12 16:23:59 +0800

    day9 q2

---
+++ day9/q2.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python3
+from input import input, test_input
+
+
+def parse_input(input):
+    intermediate = input.split()
+    return list(map(int, intermediate)) # need to be subscriptable
+
+def solution(input, target):
+    parsed_input = parse_input(input)
+    first_i = 0
+    last_i = 1
+    current_sum = parsed_input[first_i] + parsed_input[last_i]
+    while last_i < len(parsed_input):
+        if current_sum < target:
+            last_i += 1
+            current_sum += parsed_input[last_i]
+        elif current_sum > target:
+            current_sum -= parsed_input[first_i]
+            first_i += 1
+        else:
+            max_num = max(parsed_input[first_i:last_i+1])
+            min_num = min(parsed_input[first_i:last_i+1])
+            return max_num + min_num
+
+
+
+print(solution(input, 29221323))
+
+assert(solution(test_input, 127) == 62)
